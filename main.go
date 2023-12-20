ead36e8682d37479670004a97aa1b64ccf869118
Hlyan Htet <zawhlyan@gmail.com>
2023-12-20 20:52:06 +0700

    feat: add entry point for the project

--- main.go
+++ main.go
@@ -1,6 +1,63 @@
 package main
 
+import (
+	"context"
+	"os"
+
+	"github.com/joho/godotenv"
+	datamigrator "github.com/zest97/fpl-player-data-migration/data-migrator"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 func main() {
-	// Print hello world
-	println("Hello World")
+	loadEnvironmentVariables()
+
+	client := connectToMongoDB()
+
+	database := getDatabase(client)
+
+	datamigrator.Migrate(database)
+
+	defer client.Disconnect(context.Background())
+}
+
+func loadEnvironmentVariables() {
+	err := godotenv.Load()
+
+	if err != nil {
+		panic("No .env file found.")
+	}
+}
+
+func connectToMongoDB() *mongo.Client {
+	uri := os.Getenv("MONGODB_URI")
+
+	if uri == "" {
+		panic("No MONGO_URI environment variable found.")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return client
+}
+
+func getDatabase(client *mongo.Client) *mongo.Database {
+	databaseName := os.Getenv("MONGODB_DATABASE")
+
+	if databaseName == "" {
+		panic("No MONGODB_DATABASE environment variable found.")
+	}
+
+	return client.Database(databaseName)
 }
